c0ee04a66258dee6468cfc7e91778621519f177c
danyah07 <danyahal660@gmail.com>
2025-12-11 14:47:09 +0300

    Initial Commit

---
+++ Abshar/AbsharApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsharApp.swift
+//  Abshar
+//
+//  Created by Danyah ALbarqawi on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct AbsharApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Abshar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Abshar
+//
+//  Created by Danyah ALbarqawi on 11/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
